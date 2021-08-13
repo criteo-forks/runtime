473a7f577af9b68d10c2235fc97a7a21194d6f5b
John Salem <josalem@microsoft.com>
2021-08-13 14:07:51 -0700

    Handle early wakeup of counter loop (#57170)
    * Fix #56695
    * prevent a negative delta value
    
    * PR feedback + bug fix
    * bug fix is preexisting: if a user other than the one who is listening sets the interval to 0 while the counter is enabled, it makes the counter continuously submit values. Changed this so that an interval value of <= 0 results in the counter being disabled
    
    * Add assert after offline conversation

--- src/libraries/System.Private.CoreLib/src/System/Diagnostics/Tracing/CounterGroup.cs
+++ src/libraries/System.Private.CoreLib/src/System/Diagnostics/Tracing/CounterGroup.cs
@@ -123,7 +123,7 @@ namespace System.Diagnostics.Tracing
             Debug.Assert(Monitor.IsEntered(s_counterGroupLock));
             if (pollingIntervalInSeconds <= 0)
             {
-                _pollingIntervalInMilliseconds = 0;
+                DisableTimer();
             }
             else if (_pollingIntervalInMilliseconds == 0 || pollingIntervalInSeconds * 1000 < _pollingIntervalInMilliseconds)
             {
@@ -172,6 +172,7 @@ namespace System.Diagnostics.Tracing
 
         private void DisableTimer()
         {
+            Debug.Assert(Monitor.IsEntered(s_counterGroupLock));
             _pollingIntervalInMilliseconds = 0;
             s_counterGroupEnabledList?.Remove(this);
         }
@@ -237,7 +238,8 @@ namespace System.Diagnostics.Tracing
                 {
                     _timeStampSinceCollectionStarted = now;
                     TimeSpan delta = now - _nextPollingTimeStamp;
-                    if (delta > TimeSpan.Zero && _pollingIntervalInMilliseconds > 0)
+                    delta = _pollingIntervalInMilliseconds > delta.TotalMilliseconds ? TimeSpan.FromMilliseconds(_pollingIntervalInMilliseconds) : delta;
+                    if (_pollingIntervalInMilliseconds > 0)
                         _nextPollingTimeStamp += TimeSpan.FromMilliseconds(_pollingIntervalInMilliseconds * Math.Ceiling(delta.TotalMilliseconds / _pollingIntervalInMilliseconds));
                 }
             }
