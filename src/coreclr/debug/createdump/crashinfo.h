3c52021c0f4772c592087af97531b056a7f7ef70
e.zhirov <ezsilmar@gmail.com>
2022-12-22 15:31:21 +0000

    createdump: only dump committed memory (#79853)
    Dumping memory regions as they are listed in /proc/pid/maps
    results in increase of RAM usage of the target application
    on some Linux kernels.
    
    This change uses /proc/pid/pagemap to check if the page is committed
    before adding it to the regions list. As the file is not available on
    kernels 4.0 and 4.1 without elevated permissions there's a fallback to
    previous behavior.

--- src/coreclr/debug/createdump/crashinfo.h
+++ src/coreclr/debug/createdump/crashinfo.h
@@ -58,7 +58,8 @@ private:
 #else
     siginfo_t m_siginfo;                            // signal info (if any)
     bool m_canUseProcVmReadSyscall;
-    int m_fd;                                       // /proc/<pid>/mem handle
+    int m_fdMem;                                    // /proc/<pid>/mem handle
+    int m_fdPagemap;                                // /proc/<pid>/pagemap handle
 #endif
     std::string m_coreclrPath;                      // the path of the coreclr module or empty if none
 #ifdef __APPLE__
@@ -149,7 +150,8 @@ private:
     void InsertMemoryBackedRegion(const MemoryRegion& region);
     void InsertMemoryRegion(const MemoryRegion& region);
     uint32_t GetMemoryRegionFlags(uint64_t start);
-    bool ValidRegion(const MemoryRegion& region);
+    bool PageCanBeRead(uint64_t start);
+    bool PageMappedToPhysicalMemory(uint64_t start);
     void Trace(const char* format, ...);
     void TraceVerbose(const char* format, ...);
 };
