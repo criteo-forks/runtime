a04deb44c6bfea74c34b8d4c74463eb3c82cf07b
Grégoire <g.verdier@criteo.com>
2023-12-07 18:39:33 -0500

    Add EventSource event for WaitHandle waits (#94737)
    Add start/stop events for WaitHandle waits
    
    Context: #94264

--- src/coreclr/vm/syncblk.cpp
+++ src/coreclr/vm/syncblk.cpp
@@ -2843,6 +2843,7 @@ BOOL SyncBlock::Wait(INT32 timeOut)
     PendingSync   syncState(walk);
 
     OBJECTREF     obj = m_Monitor.GetOwningObject();
+    syncState.m_Object = OBJECTREFToObject(obj);
 
     m_Monitor.IncrementTransientPrecious();
 
